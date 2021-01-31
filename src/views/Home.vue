d3188d92d5de2ac71288f8ef5550a2efb961249b
Reskyppo <reskybeen@gmail.com>
2021-01-31 23:29:12 +0700

    Create CardProduct

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,26 @@
   <div class="home">
     <Navbar />
     <Hero />
+
+    <div class="row mt-4">
+      <div class="col">
+        <h2>Best <strong>Foods</strong></h2>
+      </div>
+      <div class="col">
+        <router-link
+          to="/foods"
+          class="text-secondary text-decoration-none float-right"
+        >
+          See all</router-link
+        >
+      </div>
+    </div>
+
+    <div class="row mb-3">
+      <div class="col-md-4 mt-4">
+        <CardProduct />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,6 +29,7 @@
 // @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
+import CardProduct from "@/components/CardProduct.vue";
 import Axios from "axios";
 
 Axios.get("http://localhost:3000/posts")
@@ -17,6 +38,7 @@ Axios.get("http://localhost:3000/posts")
 export default {
   name: "Home",
   components: {
+    CardProduct,
     Hero,
     Navbar,
   },
