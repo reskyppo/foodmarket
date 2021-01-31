f4e45a695a927584d59fb933070113efcc696312
Reskyppo <reskybeen@gmail.com>
2021-01-31 22:51:46 +0700

    create hero mobile version

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,23 +1,55 @@
 <template>
-  <div class="row mt-4 ml-1">
-    <div class="col">
-      <div class="d-flex h-100">
-        <div class="justify-content-center align-self-center">
-          <h2>
-            <strong>Delicious Food Menu,</strong> <br />
-            in Your Gadget
-          </h2>
-          <p>Ayo segera pesan dan pilih makanan favorit anda!</p>
-          <button class="btn border-0 btn-lg btn-success">Pesan</button>
+  <div class="hero">
+    <!-- Desktop -->
+    <div class="d-none d-md-block">
+      <div class="row mt-4 ml-1">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu,</strong> <br />
+                in Your Gadget
+              </h2>
+              <p>Ayo segera pesan dan pilih makanan favorit anda!</p>
+              <button class="btn border-0 btn-lg btn-success">Pesan</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img
+            class="ml-4"
+            src="../assets/images/Hero.png"
+            width="70%"
+            alt="illustration of Chef"
+          />
         </div>
       </div>
     </div>
-    <div class="col">
-      <img
-        src="../assets/images/Hero.png"
-        width="70%"
-        alt="illustration of Chef"
-      />
+
+    <!-- Mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4 ">
+        <div class="col-md-6 mb-4">
+          <img
+            class="ml-4"
+            src="../assets/images/Hero.png"
+            width="70%"
+            alt="illustration of Chef"
+          />
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu,</strong> <br />
+                in Your Gadget
+              </h2>
+              <p>Ayo segera pesan dan pilih makanan favorit anda!</p>
+              <button class="btn border-0 btn-lg btn-success">Pesan</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,11 @@
 // @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
+import Axios from "axios";
 
+Axios.get("http://localhost:3000/posts")
+  .then(({ data }) => console.log(data))
+  .catch((err) => console.log(err));
 export default {
   name: "Home",
   components: {
